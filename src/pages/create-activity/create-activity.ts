f6822d7baf80696265d33e068a04850e6434ecac
Manuel Entenmann <manuel.entenmann@gmail.com>
2017-05-31 10:16:12 +0200

    updated search-func

--- src/pages/create-activity/create-activity.ts
+++ src/pages/create-activity/create-activity.ts
@@ -13,6 +13,11 @@ export class CreateActivityPage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  activityPlace = {
+    lat: 0,
+    lng: 0
+  };
+  myDate: String = new Date().toISOString();
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) { }
 
@@ -70,20 +75,22 @@ export class CreateActivityPage {
 
   initAutocomplete() {
     let map = this.map;
+    let tempLatitude;
+    let tempLongitude;
     // Create the search box and link it to the UI element.
     var input = (<HTMLInputElement>document.getElementById('pac-input'));;
     var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
+    map.addListener('bounds_changed', () => {
       searchBox.setBounds(map.getBounds());
     });
 
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
-    searchBox.addListener('places_changed', function () {
+    searchBox.addListener('places_changed', () => {
       var places = searchBox.getPlaces();
 
       if (places.length == 0) {
@@ -91,14 +98,14 @@ export class CreateActivityPage {
       }
 
       // Clear out the old markers.
-      markers.forEach(function (marker) {
+      markers.forEach((marker) => {
         marker.setMap(null);
       });
       markers = [];
 
       // For each place, get the icon, name and location.
       var bounds = new google.maps.LatLngBounds();
-      places.forEach(function (place) {
+      places.forEach((place) => {
         if (!place.geometry) {
           console.log("Returned place contains no geometry");
           return;
@@ -125,8 +132,20 @@ export class CreateActivityPage {
         } else {
           bounds.extend(place.geometry.location);
         }
+
+        tempLatitude = place.geometry.location.lat();
+        tempLongitude = place.geometry.location.lng();
+        this.activityPlace.lat = tempLatitude;
+        this.activityPlace.lng = tempLongitude;
+
+        console.log(this.activityPlace);
+
       });
       map.fitBounds(bounds);
     });
+  }
+
+  createActivityOnClick() {
+    console.log("Click")
   }
 }
