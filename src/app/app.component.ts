a8ec8a11cf9bc18c1340583c256dec56fcc1ffac
Manuel Entenmann <manuel.entenmann@gmail.com>
2017-06-05 20:13:35 +0200

    delted files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      //{ title: 'Aktivität erstellen', component: CreateActivityPage },
+      { title: 'Aktivität erstellen', component: CreateActivityPage },
       { title: 'Aktivitäten', component: ViewActivityPage },
       { title: 'Kategorie', component: SelectCategoryPage },
     ];
