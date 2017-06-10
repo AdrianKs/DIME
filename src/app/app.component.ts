a9a89dca645c7d20bd7d25fb585d692ed3ea1742
Adrian Kochsiek <adrian.kochsiek@hpe.com>
2017-06-10 18:39:39 +0200

    at least something working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ import { CreateActivityPage } from "../pages/create-activity/create-activity";
 import { Utilities } from './utilities';
 import {ProfilePage} from "../pages/profile/profile";
 import { Push, PushToken } from '@ionic/cloud-angular';
+//import * as FCM from 'fcm-node';
+//var fcm = new FCM('AAAAizq6FkA:APA91bGHQhv3xZ8yz2wHF2qR-dcYTrINqlYHlQTuy1Vr1yapcJcuFA8g75tgnKasvZvg1L29b9u2c1d5tuMK7LINk8p4cjjzlOqpDANMSFLEM7h7vW5U-JPNgVuxPgBFmUC3vzLMVeXT');
 
 firebase.initializeApp(firebaseConfig);
 
@@ -90,6 +92,12 @@ export class MyApp {
       this.push.rx.notification()
         .subscribe((msg) => {
           console.log('I received awesome push: ' + msg);
+          console.log(msg);
+          firebase.database().ref('pushtest').push(msg.payload)
+            .catch((error) => {
+              console.log(error);
+            });
+          //alert(JSON.stringify(msg.payload));
         });
     });
   }
@@ -104,4 +112,26 @@ export class MyApp {
     this.authData.logout();
     this.nav.setRoot(LoginPage);
   }
+
+  /*sendPush() {
+    let deviceID = 'epzU-__USXg:APA91bHP3ieifkPMJQx-VZv5EmHoN4XP08IU2dMu1J_47H-j6FE5lZJdHVmqRETZBstXAzADSgUFWwm5kQ_kCvmThUQonsQF42s97dyMxLAnC8u_k8ta3-tjWLWg9vWQwlF_iN-E5XxD';
+    var message = {
+      to: deviceID,
+      data: {
+        title: {"locKey": "push_app_title"},
+        message: 'Simple non-localizable text for message!'
+        // Constant with formatted params
+        // message: {"locKey": "push_message_fox", "locData": ["fox", "dog"]});
+      }
+    };
+
+    fcm.send(message, function(err, response){
+      if (err) {
+        console.log(err);
+        console.log("Something has gone wrong!");
+      } else {
+        console.log("Successfully sent with response: ", response);
+      }
+    });
+  }*/
 }
